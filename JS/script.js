feeb85bbfd62179a303d0aae4e03a85000099906
lukasbuschdev <lukas.busch1996@gmx.at>
2024-02-02 15:47:00 +0100

    basic version (incl. rendering & sorting) finished

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,4 @@
-// #######################################################################
 // LOADING PAGE
-// #######################################################################
 
 async function init() {
     loadHeader();
@@ -12,5 +10,49 @@ async function getData() {
     const dataset = await ((await fetch('../database.json')).json());
     const data = dataset.orders;
 
-    log(data)
+    const categorizedOrders = categorizeOrdersByDate(data);
+    log(categorizedOrders);
+    const orders = sortOrdersByDate(categorizedOrders);
+    return orders;
+}
+
+function categorizeOrdersByDate(orders) {
+    const currentDate = new Date();
+    let moreThanAMonth = [];
+    let oneWeekToOneMonth = [];
+    let oneDayToOneWeek = [];
+
+    orders.forEach(order => {
+        const deliveryDate = parseDate(order.deliverydate);
+        const diffTime = deliveryDate.getTime() - currentDate.getTime();
+        const diffDays = Math.abs(diffTime / (1000 * 3600 * 24));
+
+        if(diffDays > 30) return moreThanAMonth.push(order);
+        if(diffDays > 7 && diffDays <= 30) return oneWeekToOneMonth.push(order);
+        if(diffDays > 1 && diffDays <= 7) return oneDayToOneWeek.push(order);       
+    });
+
+    return { moreThanAMonth, oneWeekToOneMonth, oneDayToOneWeek };
+}
+
+function parseDate(dateStr) {
+    const [day, month, year] = dateStr.split(".");
+    return new Date(year, month - 1, day);
+}
+
+function sortOrdersByDate(categorizedOrders) {
+    const parseDateForSorting = (dateStr) => {
+        const [day, month, year] = dateStr.split(".");
+        return parseInt(year + month + day, 10);
+    };
+
+    for(const category in categorizedOrders) {
+        categorizedOrders[category].sort((a, b) => {
+            const dateA = parseDateForSorting(a.deliverydate);
+            const dateB = parseDateForSorting(b.deliverydate);
+            return dateA - dateB;
+        });
+    }
+
+    return categorizedOrders;
 }
--- index/index.js
+++ index/index.js
@@ -1,20 +1,127 @@
-function loadContent() {
+async function loadContent() {
+    const data = await getData();
+
     const contentContainer = $('#content');
 
     const content = /*html*/ `
         <div class="table-header">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>> 1M</div>
+            <div>> 1W < 1M</div>
+            <div>> 1T < 1W</div>
         </div>
 
 
         <div class="table-content">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div id="urgent"></div>
+            <div id="medium"></div>
+            <div id="low"></div>
         </div>
     `;
 
     includeTemplate(contentContainer, content);
+    renderOrders(data);
 };
+
+function renderOrders(data) {
+    renderUrgent(data);
+    renderMedium(data);
+    renderLow(data);
+}
+
+
+
+// RENDER FUNCTIONS 
+
+function renderUrgent({moreThanAMonth}) {
+    // log(moreThanAMonth)
+    const urgentContainer = $('#urgent');
+    urgentContainer.innerHTML = '';
+
+    moreThanAMonth.forEach(element => {
+        urgentContainer.innerHTML += urgentTemplate(element);
+    });
+}
+
+function renderMedium({oneWeekToOneMonth}) {
+    // log(oneWeekToOneMonth)
+    const mediumContainer = $('#medium');
+    mediumContainer.innerHTML = '';
+
+    oneWeekToOneMonth.forEach(element => {
+        mediumContainer.innerHTML += urgentTemplate(element);
+    });
+}
+
+function renderLow({oneDayToOneWeek}) {
+    // log(oneDayToOneWeek)
+    const lowContainer = $('#low');
+    lowContainer.innerHTML = '';
+
+    oneDayToOneWeek.forEach(element => {
+        lowContainer.innerHTML += urgentTemplate(element);
+    });
+}
+
+
+
+// TEMPLATES
+
+function urgentTemplate({customer, ordernumber, orderdate, deliverydate}) {
+    return /*html*/ `
+        <div class="single-order-container column">
+            <div class="row">
+                <div class="txt-600">${ordernumber}</div>
+                <div>${customer.name}</div>
+            </div>
+
+            <div class="address">
+                <div>${customer.address}</div>
+            </div>
+
+            <div class="row">
+                <div>${orderdate}</div>
+                <div>${deliverydate}</div>
+            </div>
+        </div>
+    `;
+}
+
+function mediumTemplate({customer, ordernumber, orderdate, deliverydate}) {
+    return /*html*/ `
+        <div class="single-order-container column">
+            <div class="row">
+                <div class="txt-600">${ordernumber}</div>
+                <div>${customer.name}</div>
+            </div>
+
+            <div class="address">
+                <div>${customer.address}</div>
+            </div>
+
+            <div class="row">
+                <div>${orderdate}</div>
+                <div>${deliverydate}</div>
+            </div>
+        </div>
+    `;
+}
+
+function lowTemplate({customer, ordernumber, orderdate, deliverydate}) {
+    return /*html*/ `
+        <div class="single-order-container column">
+            <div class="row">
+                <div class="txt-600">${ordernumber}</div>
+                <div>${customer.name}</div>
+            </div>
+
+            <div class="address">
+                <div>${customer.address}</div>
+            </div>
+
+            <div class="row">
+                <div>${orderdate}</div>
+                <div>${deliverydate}</div>
+            </div>
+        </div>
+    `;
+}
