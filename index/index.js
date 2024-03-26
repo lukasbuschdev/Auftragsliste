75425a905c16bf0d5f2920f97e103e823aba1ef4
lukasbuschdev <lukas.busch1996@gmx.at>
2024-03-26 11:45:07 +0100

    layout changes single orders, added drafts column

--- index/index.js
+++ index/index.js
@@ -5,12 +5,14 @@ function loadContent() {
 
     const content = /*html*/ `
         <div class="table-header">
+            <div class="drafts-light">Entwürfe</div>
             <div class="urgent-light">> 1M</div>
             <div class="medium-light">> 1W < 1M</div>
             <div class="low-light">> 1T < 1W</div>
         </div>
 
         <div class="table-content">
+            <div id="drafts"></div>
             <div id="urgent"></div>
             <div id="medium"></div>
             <div id="low"></div>
@@ -22,14 +24,15 @@ function loadContent() {
 };
 
 function selectDatabase() {
-    let currentAttr = 'mla';
+    let currentAttr = 'pha';
+    // let currentAttr = 'mla';
 
     renderOrders(currentAttr);
 
-    setInterval(() => {
-        currentAttr = currentAttr === 'pha' ? 'mla' : 'pha';
-        renderOrders(currentAttr);
-    }, 5000);
+    // setInterval(() => {
+    //     currentAttr = currentAttr === 'pha' ? 'mla' : 'pha';
+    //     renderOrders(currentAttr);
+    // }, 5000);
 }
 
 
@@ -49,11 +52,6 @@ function renderTasks(data, attr) {
     ['urgent', 'medium', 'low'].forEach(category => {
         const tasks = dataSet[category];
         const container = document.getElementById(category);
-
-        if(!container) {
-            error(`Container not found for category: ${category}`);
-            return;
-        }
 
         container.innerHTML = '';
 
@@ -76,24 +74,22 @@ function renderTasks(data, attr) {
 // TEMPLATE
 
 function taskTemplate({customer, ordernumber, orderdate, deliverydate}) {
-    // const formattedAddress = customer.address.replace(/,/, ',<br>');
     const address = customer.address.split(',');
     const formattedAddress = address[1].trim();
-
 
     return /*html*/ `
         <div class="single-order-container column txt-600">
             <div class="row">
                 <div class="txt-giant">${ordernumber}</div>
-                <div class="row dates-container">
+                <div class="name-container txt-big">${customer.name}</div>
+            </div>
+            
+            <div class="row txt-normal name-address-container">
+                <div class="column dates-container">
                     <div class="txt-normal">${orderdate}</div>
                     <div class="txt-normal">${deliverydate}</div>
                 </div>
-            </div>
-
-            <div class="row txt-normal name-address-container">
-                <div>${customer.name}</div>
-                <div>${formattedAddress}</div>
+                <div class="address-container">${formattedAddress}</div>
             </div>
         </div>
     `;
